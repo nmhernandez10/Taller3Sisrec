7bcf0023bb0b80bcdde2055480b1c8da9de8b1ec
Nicolás Hernández <nm.hernandez10@uniandes.edu.co>
2019-05-21 21:16:21 -0500

    limpieza de front

---
+++ app/src/app/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    render() {
+
+
+        return (
+            <div>
+                HOLA
+            <br></br>te quiero
+
+
+            </div>
+        )
+    }
+}
--- app/src/app/index.js
+++ app/src/app/index.js
@@ -1,21 +1,7 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import { BrowserRouter,Route, Link, Switch } from "react-router-dom";
+import App from './App'
 
-import Session from './private/Session'
-import App from './public/App'
-import LogIn from './public/LogIn'
-import SignUp from './public/SignUp'
-import Profile from './private/Profile'
-
-render(
-    <BrowserRouter>
-        <Switch>
-            <Route path="/login" component = {LogIn}/>
-            <Route path="/signup" component = {SignUp}/>
-            <Route path="/session" component = {Session}/>
-            <Route path="/profile" component = {Profile}/>
-            <Route path="/" component ={App} exact/>
-        </Switch>
-    </BrowserRouter>,
+render( <App/>,
    document.getElementById('app'));
