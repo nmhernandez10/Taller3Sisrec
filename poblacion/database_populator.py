2a7543f16bbca1ba6db0b03954f4866b7de3b33e
Rogelio Garcia <r.garcia11@uniandes.edu.co>
2019-05-27 20:11:17 -0500

    populator

--- poblacion/database_populator.py
+++ poblacion/database_populator.py
@@ -1,5 +1,5 @@
 import json
-
+import re
 import requests
 
 
@@ -8,8 +8,13 @@ class DatabasePopulator:
     genome_tags_file = '../../ml-latest/genome-tags.csv'
     links_file = '../../ml-latest/links.csv'
     movies_file = '../../ml-latest/movies.csv'
+    movies_tags_file = '../../ml-latest/movies-attributes.json'
     ratings_file = '../../ml-latest/ratings.csv'
-    tag_ids_file = '../../ml-latest/tags_ids_file.json'
+    nodes_file = '../../ml-latest/salida_nodos.csv'
+
+    actors_file = '../../ml-latest/movies-actors.json'
+    directors_file = '../../ml-latest/movies-directors.json'
+
     database_address = 'http://127.0.0.1:8080/api/{}'
     # database_address = 'http://172.24.101.30:8085/api/{}'
 
@@ -38,20 +43,24 @@ class DatabasePopulator:
     def load_tags(self):
         all_tags = {}
         with open(self.genome_tags_file, 'r', encoding='utf-8') as genome_tags:
-            index = 300
+            index = 1021
             genome_tags = genome_tags.readlines()
             header = 0
-            for line in genome_tags:
+            for line in genome_tags[1020:]:
                 if header:
-                    # if index == 2:
+                    # if index == 300:
                     #     break
                     tag = line.split(',')[1]
                     tag = tag[:-1]
+                    # print(tag)
                     r = requests.post(self.database_address.format(
                         'tag'), json={'name': tag})
 
                     if r.status_code >= 300:
-                        print("Error in {}".format(index))
+                        print(
+                            "Error in {}. It's safer to start over, or from this tag.".format(index))
+                        print(tag)
+                        return
                     else:
                         all_tags[tag] = r.json()['id']
 
@@ -67,14 +76,107 @@ class DatabasePopulator:
 
             if r.status_code >= 300:
                 print("Error in {}".format(index))
+                print(tag)
+                return
             else:
                 all_tags[tag] = r.json()['id']
-
-        with open(self.tag_ids_file, 'w', encoding='utf-8') as tag_ids:
-            tag_ids.write(json.dumps(all_tags))
+            index += 1
 
     def load_movies(self):
-        pass
+        all_movies_tags = {}
+        with open(self.movies_tags_file, 'r', encoding='utf-8') as movies_tags:
+            movies_tags = movies_tags.readlines()
+            all_movies_tags = json.loads(movies_tags[0])
+        
+        movies_actors = {}
+        movies_directors = {}
+        with open(self.actors_file, 'r', encoding='utf-8') as actors:
+            actors = actors.readlines()
+            movies_actors = json.loads(actors[0])
+        with open(self.directors_file, 'r', encoding='utf-8') as directors:
+            directors = directors.readlines()
+            movies_directors = json.loads(directors[0])
+
+        # with open(self.nodes_file, 'r', encoding='ansi', errors='ignore') as nodes:
+        #     header = 0
+        #     index = 1
+        #     for line in nodes:
+        #         if header:
+        #             node = line.split(',')
+        #             actors = node[4:-1]
+        #             director = node[2]
+        #             movie_id = node[0]
+        #             empty = 0
+        #             for actor in actors:
+        #                 if actor == '':
+        #                     empty = 1
+        #             if director == '':
+        #                 empty = 1
+        #             if not empty:
+        #                 movies_actors[movie_id] = actors
+        #                 movies_directors[movie_id] = director
+        #                 if index == 58099:
+        #                     with open(self.actors_file, 'w', encoding='utf-8') as actors:
+        #                         actors.write(json.dumps(movies_actors))
+        #                     with open(self.directors_file, 'w', encoding='utf-8') as directors:
+        #                         directors.write(json.dumps(movies_directors))
+        #         else:
+        #             header = 1
+        #         index += 1
+
+        with open(self.movies_file, 'r') as movies:
+            movies = movies.readlines()
+            misfits = 0
+            for line in movies[1:]:
+                movie = line.split(',')
+                movie_title = movie[1].strip()
+                movie_year = -1
+                weird_titles = line.split('"')
+                if len(weird_titles) > 1:
+                    movie_title = weird_titles[1].strip()
+                if len(weird_titles) > 3:
+                    movie_title = ''
+                    for title_piece in weird_titles[1:-1]:
+                        movie_title += title_piece
+                    movie_title = movie_title.strip()
+                year = re.search(r"\s\(\d{4}(\W\d{4})?\)", movie_title)
+                if year:
+                    year = year.group(0)
+                    year = year[2:-1]
+                    movie_title = re.sub(
+                        r"\s\(\d{4}(\W\d{4})?\)", "", movie_title)
+                else:
+                    year = re.search(r"\s\d{4}(\W\d{4})?", movie_title)
+                    if year:
+                        year = year.group(0)
+                        year = year[1:]
+                        movie_title = re.sub(r"\s\d{4}(\W\d{4})?", "", movie_title)
+                    else:
+                        year = re.search(r"\(\d{4}(\W\d{4})?\)", movie_title)
+                        if year:
+                            year = year.group(0)
+                            year = year[2:-1]
+                            movie_title = re.sub(r"\(\d{4}(\W\d{4})?\)", "", movie_title)
+                        else:
+
+                            year = re.search(r"\s\(\d{4}(\W\s)?\)", movie_title)
+                            if year:
+                                movie_title = re.sub(r"\s\(\d{4}(\W\s)?\)", "", movie_title)
+                                year = year.group(0)
+                                year = year[2:-3]
+                            else:
+                                year = "-"
+                year = year.strip()
+                movie_title = movie_title.strip()
+                movie_id = movie[0]
+                
+                # Add to Movies
+
+                genres = all_movies_tags[movie_id]
+                for genre in genres:
+                    # Add to MovieTags
+                    pass
+            print(misfits)
 
     def load_users(self):
         pass
@@ -85,4 +187,5 @@ class DatabasePopulator:
 
 if __name__ == '__main__':
     databasePopulator = DatabasePopulator()
-    databasePopulator.load_tags()
+    # databasePopulator.load_tags()
+    databasePopulator.load_movies()
--- poblacion/tag_extractor.py
+++ poblacion/tag_extractor.py
@@ -4,8 +4,9 @@ import json
 class TagExtractor:
     genome_scores_file = '../../ml-latest/genome-scores.csv'
     genome_tags_file = '../../ml-latest/genome-tags.csv'
-    movies_tags_file = '../../ml-latest/movies_attributes.json'
+    movies_tags_file = '../../ml-latest/movies-attributes.json'
     movies_file = '../../ml-latest/movies.csv'
+    all_tags_file = '../../ml-latest/all-tags.csv'
 
     genre_ids = {'Action': 1129,
                  'Adventure': 1130,
@@ -30,33 +31,9 @@ class TagExtractor:
                  }
 
     def define_tags(self):
-        movies = {}
+        all_movies_tags = {}
         with open(self.movies_tags_file, 'w', encoding='utf-8') as movie_tags:
             movie_tags.write('')
-
-        movies_genres = {}
-        with open(self.movies_file, 'r', encoding='utf-8') as movies:
-            index = 0
-            header = 0
-            movies = movies.readlines()
-            print(len(movies))
-            for line in movies[1:]:
-                # if index == 2:
-                #     break
-                # if header:
-                movie_info = line.split(',')
-
-                movie_id = movie_info[0]
-                movie_genres = movie_info[-1][:-1].split('|')
-                movies_genres[movie_id] = [self.genre_ids[name]
-                                            for name in movie_genres]
-                header = 1
-                index += 1
-                if index % 10000 == 0:
-                    print('Reading movies file to fetch genres: {:.2f}% done.'.format(
-                        index/len(movies)*100))
-
-        # print(movies_genres)
 
         average_tags = 0
         number_of_movies = 0
@@ -76,35 +53,75 @@ class TagExtractor:
                 score = genome_score[2][:-1]
                 if last_movie_id != movie_id:
                     if last_movie_id != -1:
-                        average_tags += len(movies[last_movie_id])
+                        average_tags += len(all_movies_tags[last_movie_id])
                         number_of_movies += 1
-                        for genre in movies_genres[last_movie_id]:
-                            movies[last_movie_id].append(genre)
-                        with open(self.movies_tags_file, 'a', encoding='utf-8') as movie_attributes:
-                            movie_tags.write(json.dumps(movies))
-                            movie_tags.write('\n')
                     last_movie_id = movie_id
-                    movies = {}
+                    
                 try:
                     if float(score) >= 0.65:
-                        movies[movie_id].append(tag_id)
+                        all_movies_tags[movie_id].append(tag_id)
                 except:
-                    movies[movie_id] = []
+                    all_movies_tags[movie_id] = []
                     if float(score) >= 0.65:
-                        movies[movie_id].append(tag_id)
+                        all_movies_tags[movie_id].append(tag_id)
                 try:
-                    movies[movie_id]
+                    all_movies_tags[movie_id]
                 except:
-                    movies[movie_id] = []
+                    all_movies_tags[movie_id] = []
                 index += 1
                 header = 1
 
                 if index % 500000 == 0:
                     print('Reading genome scores file: {:.2f}% done.'.format(
                         index/len(genome_scores)*100))
+        # print(movies)
         print(average_tags/number_of_movies)
 
+        movies_genres = {}
+        with open(self.movies_file, 'r', encoding='utf-8') as movies:
+            index = 0
+            header = 0
+            movies = movies.readlines()
+            print(len(movies))
+            for line in movies[1:]:
+                # if index == 2:
+                #     break
+                # if header:
+                movie_info = line.split(',')
+
+                movie_id = movie_info[0]
+                movie_genres = movie_info[-1][:-1].split('|')
+                movies_genres[movie_id] = [self.genre_ids[name]
+                                            for name in movie_genres]
+
+                try:
+                    for movie_tag in all_movies_tags[movie_id]:
+                        movies_genres[movie_id].append(movie_tag)
+                except:
+                    print('The movie with id {} is not present in the list of movies'.format(movie_id))
+
+                header = 1
+                index += 1
+                if index % 10000 == 0:
+                    print('Reading movies file to fetch genres: {:.2f}% done.'.format(
+                        index/len(movies)*100))
+
+        # print(movies_genres)
+        with open(self.movies_tags_file, 'w', encoding='utf-8') as movies_tags:
+            movies_tags.write(json.dumps(movies_genres))
+
+    def append_tag_ids(self):
+        with open(self.genome_tags_file, 'r', encoding='utf-8') as genome_tags:
+            genome_tags = genome_tags.readlines()
+            with open(self.all_tags_file, 'w', encoding='utf-8') as all_tags:
+                all_tags.write('')
+                for line in genome_tags:
+                    genome_tag = line.split(',')
+                    all_tags.write('{},{}'.format(genome_tag[0], genome_tag[1]))
+                for genre in self.genre_ids:
+                    all_tags.write('{},{}\n'.format(self.genre_ids[genre], genre))
 
 if __name__ == '__main__':
     tagExtractor = TagExtractor()
+    # tagExtractor.append_tag_ids()
     tagExtractor.define_tags()
