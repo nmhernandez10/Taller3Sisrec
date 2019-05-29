59a1f181fe45e62e4b84b7604eeea5b9780461a6
Nicolás Hernández <nm.hernandez10@uniandes.edu.co>
2019-05-29 13:00:03 -0500

    wtf

--- app/src/app/private/Session.js
+++ app/src/app/private/Session.js
@@ -211,7 +211,6 @@ export default class Session extends Component {
 
     updateOnlineModels = () => {
         fetch('http://172.24.101.30:8081/ontological/' + this.state.user.id, { mode: 'no-cors' }).then(res => {
-
             fetch('http://172.24.101.30:8081/collaborative/' + this.state.user.id, { mode: 'no-cors' }).then(res => {
                 this.setState({
                     update: true
